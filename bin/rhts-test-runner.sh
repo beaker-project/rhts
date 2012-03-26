e9160209f0228426e2f2e153c61fc7a77e891208
Bill Peck <bpeck@redhat.com>
2012-03-26 10:56:35 -0400

    rhts-test-runner overrides PATH adjusted by tortilla wrapper
    Bug: 806806

--- bin/rhts-test-runner.sh
+++ bin/rhts-test-runner.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export PATH=/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/X11R6/bin:/root/bin
+export PATH=$PATH:/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/X11R6/bin:/root/bin
 
 function sizeof() {
     # sizeof FILE -- returns FILE's size in bytes
